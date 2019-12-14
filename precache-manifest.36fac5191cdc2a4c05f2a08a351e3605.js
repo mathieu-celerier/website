84074815a4015bcf9cea496db98499c705460e69
Mathieu Celerier <mathieu.celerier.ing@gmail.com>
2019-12-14 10:52:57 +0100

    Updates

---
+++ precache-manifest.36fac5191cdc2a4c05f2a08a351e3605.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f215fd309faed2f96554be4013e391f4",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "bd7c64eeac6c080f76b9",
+    "url": "/website/static/css/main.b9fb8596.chunk.css"
+  },
+  {
+    "revision": "091ddedbdc667ec05a97",
+    "url": "/website/static/js/2.ce6ebb28.chunk.js"
+  },
+  {
+    "revision": "bd7c64eeac6c080f76b9",
+    "url": "/website/static/js/main.06fb3041.chunk.js"
+  },
+  {
+    "revision": "34b0665d3b3f5080e9c6",
+    "url": "/website/static/js/runtime-main.19708152.js"
+  },
+  {
+    "revision": "d6bfc188768fffb3902a459e2226d9a5",
+    "url": "/website/static/media/AI.d6bfc188.jpg"
+  },
+  {
+    "revision": "b10fe1c56007d51bcf6be92cae4fc29a",
+    "url": "/website/static/media/Embedded.b10fe1c5.jpg"
+  },
+  {
+    "revision": "81cc224b6c5f572604d31566fbab6c0a",
+    "url": "/website/static/media/Gravity-Bold.81cc224b.otf"
+  },
+  {
+    "revision": "c5833b37f54a60b621af207ee1748141",
+    "url": "/website/static/media/Gravity-Regular.c5833b37.otf"
+  },
+  {
+    "revision": "74752ed2cefbd7c20296e62380a781db",
+    "url": "/website/static/media/Logo_Jour.74752ed2.png"
+  },
+  {
+    "revision": "7fa187c73e672846256ee4a7747ff8f1",
+    "url": "/website/static/media/Robotic-Vector.7fa187c7.jpg"
+  },
+  {
+    "revision": "122e627ad90199de1180c837c4134e15",
+    "url": "/website/static/media/Web-Dev.122e627a.jpg"
+  },
+  {
+    "revision": "fadf5b8e22d317fe6c9f58b2537e86a9",
+    "url": "/website/static/media/main_wallpaper.fadf5b8e.jpg"
+  },
+  {
+    "revision": "85b3a637eeff1209f11a424d1a28006b",
+    "url": "/website/static/media/my-work.85b3a637.png"
+  }
+]);
